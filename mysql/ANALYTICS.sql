875d432e6001a33ba5e2b002e2f030ba0ab6aa63
Sharan Thakur <sharandt19@live.com>
2025-03-24 20:27:03 +0100

    Refactor SQL queries for improved readability and add genre preferences analysis

--- mysql/ANALYTICS.sql
+++ mysql/ANALYTICS.sql
@@ -2,82 +2,76 @@
 
 
 -- 5 most popular games
-SELECT 
+SELECT
     v.title,
     v.genre,
     COUNT(gp.purchase_id) AS times_purchased
 FROM
     VideoGames v
-INNER JOIN
-    Purchases gp ON v.game_id = gp.game_id
+    INNER JOIN Purchases gp ON v.game_id = gp.game_id
 GROUP BY
-    v.title, v.genre
+    v.title,
+    v.genre
 ORDER BY
     times_purchased DESC
-LIMIT 5;
-
+LIMIT
+    5;
 
 -- gamers with most purchases
-SELECT 
+SELECT
     g.gamer_tag,
     g.email,
     COUNT(gp.purchase_id) AS games_purchased,
     SUM(gp.price_paid) AS total_spent
 FROM
     Gamers g
-INNER JOIN
-    Purchases gp ON g.gamer_tag = gp.gamer_tag
+    INNER JOIN Purchases gp ON g.gamer_tag = gp.gamer_tag
 GROUP BY
-    g.gamer_tag, g.email
+    g.gamer_tag,
+    g.email
 ORDER BY
     games_purchased DESC;
 
-
 -- gamers with max spending
-SELECT 
+SELECT
     g.gamer_tag,
     g.email,
     COUNT(gp.purchase_id) AS games_purchased,
     SUM(gp.price_paid) AS total_spent
 FROM
     Gamers g
-INNER JOIN
-    Purchases gp ON g.gamer_tag = gp.gamer_tag
+    INNER JOIN Purchases gp ON g.gamer_tag = gp.gamer_tag
 GROUP BY
-    g.gamer_tag, g.email
+    g.gamer_tag,
+    g.email
 ORDER BY
     total_spent DESC;
 
-
 -- gamers with 0 spending
-SELECT 
+SELECT
     g.gamer_tag,
     g.email
 FROM
     Gamers g
-LEFT JOIN
-    Purchases p ON g.gamer_tag = p.gamer_tag
+    LEFT JOIN Purchases p ON g.gamer_tag = p.gamer_tag
 WHERE
     p.purchase_id IS NULL
 GROUP BY
     g.gamer_tag;
-    
 
 -- games that have not been purchased
-SELECT 
-	v.game_id,
+SELECT
+    v.game_id,
     v.title,
     v.genre
 FROM
     VideoGames v
-LEFT JOIN
-    Purchases gp ON v.game_id = gp.game_id
+    LEFT JOIN Purchases gp ON v.game_id = gp.game_id
 WHERE
     gp.purchase_id IS NULL;
 
-
 -- top 5 most expensive games
-SELECT 
+SELECT
     v.title,
     v.genre,
     v.price
@@ -85,11 +79,11 @@ FROM
     VideoGames v
 ORDER BY
     v.price DESC
-LIMIT 5;
-
+LIMIT
+    5;
 
 -- top 5 cheapest games
-SELECT 
+SELECT
     v.title,
     v.genre,
     v.price
@@ -97,20 +91,35 @@ FROM
     VideoGames v
 ORDER BY
     v.price
-LIMIT 5;
-
+LIMIT
+    5;
 
 -- publishers with their game count
-SELECT 
+SELECT
     gp.publisher_id,
     gp.name,
     gp.country,
     COUNT(v.game_id) AS games_published
 FROM
     GamePublishers gp
-INNER JOIN
-    VideoGames v ON gp.publisher_id = v.publisher_id
+    INNER JOIN VideoGames v ON gp.publisher_id = v.publisher_id
 GROUP BY
     gp.publisher_id
 ORDER BY
     games_published DESC;
+
+-- Genre preferences by email domain
+SELECT
+    SUBSTRING_INDEX (g.email, '@', -1) AS email_domain,
+    v.genre,
+    COUNT(*) AS purchase_count
+FROM
+    Gamers g
+    INNER JOIN Purchases p ON g.gamer_tag = p.gamer_tag
+    INNER JOIN VideoGames v ON p.game_id = v.game_id
+GROUP BY
+    email_domain,
+    v.genre
+ORDER BY
+    email_domain,
+    purchase_count DESC
