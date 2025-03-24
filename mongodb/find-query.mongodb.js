bbc87102a9ecd537d754d347ff9e00c3c240333a
Sharan Thakur <sharandt19@live.com>
2025-03-25 00:01:45 +0100

    Refactor MongoDB queries for consistency and clarity in story retrieval

--- mongodb/find-query.mongodb.js
+++ mongodb/find-query.mongodb.js
@@ -5,32 +5,40 @@
 use("mongo_stories");
 
 // Find all documents.
+use("mongo_stories");
 db.stories.find();
 
-// Find all documents with the title "Belling the Cat".
-db.stories.find({ title: "Belling the Cat" });
+// Find all documents with the title "The Fox and The Grapes".
+use("mongo_stories");
+db.stories.find({ title: "The Fox and The Grapes" });
 
 // Find recent stories.
+use("mongo_stories");
 db.stories.find().sort({ datetime: -1 }).limit(5);
 
 // Find stories before 18 April 2024
+use("mongo_stories");
 db.stories.find({ datetime: { $lt: "2024-04-18" } });
 
 // Find stories after 18 April 2024
+use("mongo_stories");
 db.stories.find({ datetime: { $gt: "2024-04-18" } });
 
 // Find random one story between 15 Jan 2024 and 18 April 2024
+use("mongo_stories");
 db.stories.aggregate([
   {
     $match: {
       datetime: { $gte: "2024-01-15", $lte: "2024-04-18" },
     },
   },
-  { $sample: { size: 1 } }
+  { $sample: { size: 1 } },
 ]);
 
 // Find all authors
+use("mongo_stories");
 db.stories.distinct("author");
 
 // Find all Stories by author "Rajesh Kumar Verma"
+use("mongo_stories");
 db.stories.find({ author: "Rajesh Kumar Verma" });
